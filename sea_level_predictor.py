99b3699e19015f995489382a74700fb2eedc56a3
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-09-19 21:57:47 -0300

    fix: correção dos coments dos codigo

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,21 +3,31 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    # Read data from file
+    # Carregar os dados
+    data = pd.read_csv('epa-sea-level.csv')
 
+    # Gráfico de dispersão
+    plt.scatter(data['Year'], data['CSIRO Adjusted Sea Level'])
 
-    # Create scatter plot
+    # Linha de tendência completa
+    full_fit = linregress(data['Year'], data['CSIRO Adjusted Sea Level'])
+    years_full = range(data['Year'].min(), 2051)
+    sea_level_full = full_fit.intercept + full_fit.slope * pd.Series(years_full)
+    plt.plot(years_full, sea_level_full, label='Trend: All Data')
 
+    # isso aqui mostra a linha de tendência desde 2000
+    recent_data = data[data['Year'] >= 2000]
+    recent_fit = linregress(recent_data['Year'], recent_data['CSIRO Adjusted Sea Level'])
+    years_recent = range(2000, 2051)
+    sea_level_recent = recent_fit.intercept + recent_fit.slope * pd.Series(years_recent)
+    plt.plot(years_recent, sea_level_recent, label='Trend: From 2000')
 
-    # Create first line of best fit
+    # mostra os rótulos e títulos
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.legend()
 
-
-    # Create second line of best fit
-
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
+    # pra salvar gráfico como img
     plt.savefig('sea_level_plot.png')
     return plt.gca()
